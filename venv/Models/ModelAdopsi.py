338cfef1a625238feef3f71fe9c81a97ee4f0334
Len112 <ellen.pratama.ep@gmail.com>
2021-06-14 01:32:22 +0700

    adopsi fungsional

--- venv/Models/ModelAdopsi.py
+++ venv/Models/ModelAdopsi.py
@@ -15,42 +15,37 @@ class Adopsi:
 
         # Fetch all the row from a tkucing table using fetchall() method
     def fetch_all(self):
+
         # execute query in mysql database tteam
-        self.cursor.execute('SELECT * FROM tkucing')
+        self.cursor.execute('SELECT tpengadopsi.idadopsi,tpengadopsi.noidentitaspengadopsi,tpengadopsi.namapengadopsi,tpengadopsi.emailpengadopsi,tpengadopsi.teleponpengadopsi,tpengadopsi.jeniskelamin,tpengadopsi.alamatpengadopsi,tpengadopsi.alasanadopsi,tpengadopsi.statusadopsi,tpengadopsi.idkucing,tkucing.namakucing,tkucing.jeniskucing,tkucing.photokucing  FROM `tpengadopsi` JOIN `tkucing` WHERE tpengadopsi.idkucing=tkucing.idkucing')
         # return result from fetchall method
         return self.cursor.fetchall()
 
         # Fetch a single row tpegawai using fetchall() method.
     def fetch_one(self, id):
         # execute query select one in mysql database tteam
-        self.cursor.execute('SELECT * FROM tkucing WHERE idkucing = %s',(id,))
+        self.cursor.execute('SELECT * FROM tpengadopsi WHERE idadopsi = %s',(id,))
         # return result from fetchone method
         return self.cursor.fetchall()
 
-        #insert data into tkucing table
+        #insert data into tpengadopsi table
     def create(self, adopsi=dict({})):
         # execute query insert in mysql database tpegawai
-        self.cursor.execute('INSERT INTO tpengadopsi(idadopsi,namapengadopsi,emailpengadopsi,teleponpengadopsi,jeniskelamin,alamatpengadopsi,alasanadopsi,statusadopsi,idkucing) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)',
-                            (adopsi['idadopsi'],adopsi['namapengadopsi'],adopsi['emailpengadopsi'],adopsi['teleponpengadopsi'],adopsi['jeniskelamin'],adopsi['alasanadopsi'],adopsi['alasanadopsi'],adopsi['statusadopsi'],adopsi['idkucing']))
+        self.cursor.execute('INSERT INTO tpengadopsi(idadopsi,noidentitaspengadopsi,namapengadopsi,emailpengadopsi,teleponpengadopsi,jeniskelamin,alamatpengadopsi,alasanadopsi,statusadopsi,idkucing) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+                            (adopsi['idadopsi'],adopsi['noidentitaspengadopsi'],adopsi['namapengadopsi'],adopsi['emailpengadopsi'],adopsi['teleponpengadopsi'],adopsi['jeniskelamin'],adopsi['alamatpengadopsi'],adopsi['alasanadopsi'],adopsi['statusadopsi'],adopsi['idkucing']))
         # Commit your changes in the database
         self.conn.commit()
 
-       # update data into tkucing table
-    def update(self, kucing=dict({})):
-        # execute query update in mysql database tpegawai
-        self.cursor.execute('UPDATE tkucing SET namakucing= %s,jeniskucing= %s ,usiakucing=%s,ukurankucing=%s , jeniskelamin= %s , photokucing= %s, tentangkucing=%s, statuskucing=%s WHERE idkucing= %s',
-                            (kucing['namakucing'],kucing['jeniskucing'],kucing['usiakucing'],kucing['ukurankucing'], kucing['jeniskelamin'], kucing['photokucing'],
-                             kucing['tentangkucing'], kucing['statuskucing'], kucing['idkucing']))
+        #update data into tpengadopsi tabel
+    def update(self, adopsi=dict({})):
+        # execute query insert in mysql database tpegawai
+        self.cursor.execute('UPDATE tpengadopsi SET statusadopsi=%s WHERE idkucing=%s',(adopsi['statusadopsi'],adopsi['idkucing']))
         # Commit your changes in the database
         self.conn.commit()
 
 
 
-    def delete(self, id):
-        # execute query delete in mysql database tkucing
-        self.cursor.execute('DELETE FROM tkucing WHERE idkucing = %s', (id,))
-        # Commit your changes in the database
-        self.conn.commit()
+
 
 
 
